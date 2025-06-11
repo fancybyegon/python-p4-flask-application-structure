7f9e74bddec6b47558f75dbd7a7d8105b25424b2
Fancy Chepngetich Byegon <fancychepngetichbyegon@gmail.com>
2025-06-11 19:21:00 +0300

    flask application structure

--- server/app.py
+++ server/app.py
@@ -5,7 +5,10 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return '<h1>Welcome to my page!</h1>'
-# append to server/app.py
+
+@app.route('/<string:username>')
+def user(username):
+    return f'<h1>Profile for {username}</h1>'
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
